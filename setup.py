87ec2177556b6b2f62730720279fb5e5f4858ce4
Santosh Passoubady <61618641+ssantoshp@users.noreply.github.com>
2021-03-08 23:26:39 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Santosh Passoubady',                   # Type in your name
   author_email = 'your.email@domain.com',      # Type in your E-Mail
   url = 'https://github.com/ssantoshp/trafalgar',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ssantoshp/trafalgar/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['Portfolio management', 'Quantitative finance', 'Efficient frontier', 'Stock analysis', 'Porfolio analysis' ],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'tabulate',
