20e9471cee54bd7473dc9f8e332b32acffe77e9f
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-06-01 17:27:26 +0200

    Update empyrial.py

--- Empyrial/empyrial.py
+++ Empyrial/empyrial.py
@@ -7,6 +7,13 @@ from pandas_datareader import data as web
 import datetime as dt
 from empyrical import*
 import quantstats as qs
+from darts.models import*
+from darts import TimeSeries
+from darts.utils.missing_values import fill_missing_values
+from darts.metrics import mape, mase
+import logging
+import warnings
+from warnings import filterwarnings
 
 
 today = dt.date.today()
@@ -289,3 +296,116 @@ def empyrial(my_portfolio, rf=0.0, sigma_value=1, confidence_value=0.95):
 
 
   return qs.plots.returns(returns,benchmark, cumulative=True), qs.plots.monthly_heatmap(returns), qs.plots.drawdown(returns), qs.plots.drawdowns_periods(returns), qs.plots.rolling_volatility(returns), qs.plots.rolling_sharpe(returns), qs.plots.rolling_beta(returns, benchmark)
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def oracle(my_portfolio, prediction_days=30, based_on='Adj Close'):
+  logger = logging.getLogger()
+  warnings.simplefilter(action='ignore', category=FutureWarning)
+  filterwarnings('ignore')
+  
+  logging.disable(logging.INFO)
+
+  mape_df = pd.DataFrame()
+  mape_df = mape_df.append({'Exponential smoothing' : 0, 'Prophet' : 0, 'Auto-ARIMA' :  0, 'Theta(2)':0, 'ARIMA' : 0, 'FFT' : 0, 'FourTheta' :  0, 'NaiveDrift':0, 'NaiveMean' :  0, 'NaiveSeasonal':0 }, 
+                ignore_index = True)
+
+  final_df = pd.DataFrame()
+  final_df = final_df.append({'Exponential smoothing' : 0, 'Prophet' : 0, 'Auto-ARIMA' :  0, 'Theta(2)':0, 'ARIMA' : 0, 'FFT' : 0, 'FourTheta' :  0, 'NaiveDrift':0, 'NaiveMean' :  0, 'NaiveSeasonal':0 },
+                ignore_index = True)
+
+  for asset in my_portfolio.portfolio:
+
+    result = pd.DataFrame()
+    def eval_model(model):
+      model.fit(train)
+      forecast = model.predict(len(val))
+      result[model] = [mape(val, forecast)]
+
+    prediction = pd.DataFrame()
+    def predict(model):
+      model.fit(train)
+      forecast = model.predict(len(val))
+      pred = model.predict(prediction_days)
+
+      b = [str(pred[-1])]
+      b = [words for segments in b for words in segments.split()]
+      b = float(b[2])
+      prediction[model] = [str(round(((b-start_value)/start_value)*100,3))+' %']
+
+
+    df = web.DataReader(asset, data_source='yahoo', start = my_portfolio.start_date, end= my_portfolio.end_date)
+    df = pd.DataFrame(df)
+    df.reset_index(level=0, inplace=True)
+
+    series = TimeSeries.from_dataframe(df, 'Date', based_on, freq='D')
+    series = fill_missing_values(series)
+
+    train_index = round(len(df.index)*0.7)
+    train_date = df.loc[[train_index]]['Date'].values
+    date = str(train_date[0])[:10]
+    date = date.replace('-', '') 
+    timestamp = date+'000000'
+    train, val = series.split_before(pd.Timestamp(timestamp))
+    eval_model(ExponentialSmoothing())
+    eval_model(Prophet())
+    eval_model(AutoARIMA())
+    eval_model(Theta())
+    eval_model(ARIMA())
+    eval_model(FFT())
+    eval_model(FourTheta())
+    eval_model(NaiveDrift())
+    eval_model(NaiveMean())
+    eval_model(NaiveSeasonal())
+    result.columns = ['Exponential smoothing','Prophet', 'Auto-ARIMA', 'Theta(2)', 'ARIMA', 'FFT','FourTheta','NaiveDrift','NaiveMean', 'NaiveSeasonal']
+    result.index = [asset]
+    mape_df = pd.concat([result, mape_df])
+    start_pred = str(df["Date"].iloc[-2])[:10]
+    start_value = df[based_on].iloc[-2]
+    start_pred = start_pred.replace('-', '') 
+    timestamp = start_pred+'000000'
+    train, val = series.split_before(pd.Timestamp(timestamp))
+
+    predict(ExponentialSmoothing())
+    predict(Prophet())
+    predict(AutoARIMA())
+    predict(Theta())
+    predict(ARIMA())
+    predict(FFT())
+    predict(FourTheta())
+    predict(NaiveDrift())
+    predict(NaiveMean())
+    predict(NaiveSeasonal())
+
+    prediction.columns = ['Exponential smoothing','Prophet', 'Auto-ARIMA', 'Theta(2)', 'ARIMA', 'FFT','FourTheta','NaiveDrift','NaiveMean', 'NaiveSeasonal']
+    prediction.index = [asset]
+    final_df = pd.concat([prediction, final_df])
+
+  print("Assets MAPE (accuracy score)")
+  with pd.option_context('display.max_rows', None, 'display.max_columns', None) and pd.option_context('expand_frame_repr', False):
+    print(mape_df.iloc[:-1,:])
+  mape_df = pd.DataFrame(mape_df.iloc[:-1,:])
+  print("\n")
+  print("Assets returns prediction for the next "+str(prediction_days)+" days")
+  with pd.option_context('display.max_rows', None, 'display.max_columns', None) and pd.option_context('expand_frame_repr', False):
+    print(final_df.iloc[:-1,:])
+  final_df = pd.DataFrame(final_df.iloc[:-1,:])
+  
+  portfolio_pred = pd.DataFrame()
+
+  for column in final_df.columns:
+    rets = []
+    for index in final_df.index:
+      place = my_portfolio.portfolio.index(index)
+      returns = float(final_df[column][index][:-1])
+      wts = my_portfolio.weights[my_portfolio.portfolio.index(index)]
+      ret = (returns*wts)
+      rets.append(ret)
+    portfolio_pred[column] = rets
+    portfolio_pred[column] = portfolio_pred[column].sum()
+
+  print("\n")
+  print("Portfolio returns prediction for the next"+str(prediction_days)+" days")
+  display(portfolio_pred.iloc[0])
+  
+
+  logger.disabled = False
