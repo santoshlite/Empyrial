8bdea205204ccc71377f8fedcc252f136e81c9df
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-06-27 02:16:43 +0200

    🧪 Move to test

---
+++ Empyrial/test/fundlens_test.py
@@ -0,0 +1,8 @@
+from empyrial import fundlens, Engine
+
+portfolio = Engine(    
+                  start_date= "2020-06-09", 
+                  portfolio= ["BABA", "RELIANCE.NS", "KO", "^DJI","^IXIC"]
+)
+
+fundlens(portfolio)
