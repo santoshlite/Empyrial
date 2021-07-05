ad3436c476aff05ed423f3d4f4b09606d6dfd966
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-07-05 12:39:59 +0200

    Delete fundlens_test.py

--- Empyrial/test/fundlens_test.py
+++
@@ -1,8 +0,0 @@
-from empyrial import fundlens, Engine
-
-portfolio = Engine(    
-                  start_date= "2020-06-09", 
-                  portfolio= ["BABA", "RELIANCE.NS", "KO", "^DJI","^IXIC"]
-)
-
-fundlens(portfolio)
