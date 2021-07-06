ad6cc026ffeec10c7397ce722b965bfe1db314bb
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-07-07 01:30:58 +0200

    🏂 New stuff

--- Empyrial/empyrial.py
+++ Empyrial/empyrial.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
 import math
-import matplotlib.pyplot as plt
-import yfinance as yf
 import datetime as dt
 from empyrical import*
 import quantstats as qs
@@ -11,6 +9,7 @@ from IPython.display import display
 import matplotlib.pyplot as plt
 import copy
 import yfinance as yf
+from fpdf import FPDF
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -119,6 +118,7 @@ def empyrial(my_portfolio, rf=0.0, sigma_value=1, confidence_value=0.95, rebalan
       
       #then our rebalancing dates into that list 
       dates = dates + rebalance_schedule.columns.to_list()
+
 
       datess = []
       for date in dates:
@@ -848,3 +848,314 @@ def make_rebalance(start_date, end_date, optimizer, portfolio_input, rebalance, 
     
     make_rebalance.output = output_df
     return output_df
+#--------------------------------------------------------------------------------------------------------
+def get_report(my_portfolio, rf=0.0, sigma_value=1, confidence_value=0.95, rebalance=False):
+
+  #standard emyrial output
+  if rebalance == False:
+      
+      #standard returns calculation
+      returns = get_returns(my_portfolio.portfolio, my_portfolio.weights, start_date=my_portfolio.start_date,end_date=my_portfolio.end_date)
+      
+  #when we want to do the rebalancing
+  if rebalance == True:
+      
+      print("")
+      print('rebalance hit')
+      
+      #we want to get the dataframe with the dates and weights
+      rebalance_schedule = my_portfolio.rebalance
+
+      columns = []
+      for date in rebalance_schedule.columns:
+            date = date[0:10]
+            columns.append(date)
+      rebalance_schedule.columns = columns
+      
+      #then want to make a list of the dates and start with our first date
+      dates = [my_portfolio.start_date]
+      
+      #then our rebalancing dates into that list 
+      dates = dates + rebalance_schedule.columns.to_list()
+
+      datess = []
+      for date in dates:
+          date = date[0:10]
+          datess.append(date)
+      dates = datess
+      #this will hold returns
+      returns = pd.Series()
+      
+  
+      #then we want to be able to call the dates like tuples
+      for i in range(len(dates) - 1):
+
+          #get our weights
+          weights = rebalance_schedule[str(dates[i+1])]
+          
+          #then we want to get the returns
+          add_returns = get_returns(my_portfolio.portfolio, weights, start_date = dates[i], end_date = dates[i+1])
+          
+          #then append those returns
+          returns = returns.append(add_returns)
+
+  creturns = (returns + 1).cumprod()
+
+  #risk manager
+  try:
+    if list(my_portfolio.risk_manager.keys())[0] == "Stop Loss":
+
+      values = []
+      for r in creturns:
+        if r <= 1+my_portfolio.risk_manager['Stop Loss']:
+          values.append(r)
+        else:
+          pass
+
+      try:
+        date = creturns[creturns == values[0]].index[0]
+        date = str(date.to_pydatetime())
+        my_portfolio.end_date = date[0:10]
+        returns = returns[:my_portfolio.end_date]
+
+      except Exception as e:
+        pass
+
+    if list(my_portfolio.risk_manager.keys())[0] == "Take Profit":
+
+      values = []
+      for r in creturns:
+        if r >= 1+my_portfolio.risk_manager['Take Profit']:
+          values.append(r)
+        else:
+          pass
+      
+      try:
+        date = creturns[creturns == values[0]].index[0]
+        date = str(date.to_pydatetime())
+        my_portfolio.end_date = date[0:10]
+        returns = returns[:my_portfolio.end_date]
+
+      except Exception as e:
+        pass
+
+    if list(my_portfolio.risk_manager.keys())[0] == "Max Drawdown":
+
+      drawdown = qs.stats.to_drawdown_series(returns)
+
+      values = []
+      for r in drawdown:
+        if r <= my_portfolio.risk_manager['Max Drawdown']:
+          values.append(r)
+        else:
+          pass
+
+      try:
+        date = drawdown[drawdown == values[0]].index[0]
+        date = str(date.to_pydatetime())
+        my_portfolio.end_date = date[0:10]
+        returns = returns[:my_portfolio.end_date]
+
+      except Exception as e:
+        pass
+
+  except Exception as e:
+      pass
+
+  fig1, ax1 = plt.subplots()
+  fig1.set_size_inches(5, 5)
+  cs= ['#ff9999','#66b3ff','#99ff99','#ffcc99', '#f6c9ff', '#a6fff6', '#fffeb8', '#ffe1d4', '#cccdff', '#fad6ff']
+  ax1.pie(my_portfolio.weights, labels=my_portfolio.portfolio, autopct='%1.1f%%',
+          shadow=False, colors=cs)
+  ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+  plt.rcParams['font.size'] = 12
+  plt.close(fig1)
+  fig1.savefig('allocation.png')
+
+  pdf = FPDF()
+  pdf.add_page()
+  pdf.set_font('arial', 'B', 14)
+  pdf.image('https://user-images.githubusercontent.com/61618641/120909011-98f8a180-c670-11eb-8844-2d423ba3fa9c.png', x = None, y = None, w = 45, h = 5, type = '', link = 'https://github.com/ssantoshp/Empyrial')
+  pdf.cell(20, 15, f"Report", ln=1)
+  pdf.set_font('arial', size=11)
+  pdf.image('allocation.png', x = 135, y = 0, w = 70, h = 70, type = '', link = '')
+  pdf.cell(20, 7, f"Start date: " + str(my_portfolio.start_date), ln=1)
+  pdf.cell(20, 7, f"End date: " + str(my_portfolio.end_date), ln=1)
+
+
+  benchmark = get_returns(my_portfolio.benchmark, wts=[1], start_date=my_portfolio.start_date,end_date=my_portfolio.end_date)
+
+  CAGR = cagr(returns, period=DAILY, annualization=None)
+  CAGR = round(CAGR,2)
+  CAGR = CAGR.tolist()
+  CAGR = str(round(CAGR*100,2)) + '%'
+
+
+
+  CUM = cum_returns(returns, starting_value=0, out=None)*100
+  CUM = CUM.iloc[-1]
+  CUM = CUM.tolist()
+  CUM = str(round(CUM,2)) + '%'
+
+
+  VOL = qs.stats.volatility(returns, annualize=True, trading_year_days=252)
+  VOL = VOL.tolist()
+  VOL = str(round(VOL*100,2))+' %'
+
+
+  SR = qs.stats.sharpe(returns, rf=rf)
+  SR = np.round(SR, decimals=2)
+  SR = str(SR)
+
+  empyrial.SR = SR
+
+  CR =  qs.stats.calmar(returns)
+  CR = CR.tolist()
+  CR = str(round(CR,2))
+
+  empyrial.CR = CR
+
+  STABILITY = stability_of_timeseries(returns)
+  STABILITY = round(STABILITY,2)
+  STABILITY = str(STABILITY)
+
+
+  MD = max_drawdown(returns, out=None)
+  MD = str(round(MD*100,2))+' %'
+
+  
+  '''OR = omega_ratio(returns, risk_free=0.0, required_return=0.0)
+  OR = round(OR,2)
+  OR = str(OR)
+  print(OR)'''
+
+  SOR = sortino_ratio(returns, required_return=0, period=DAILY)
+  SOR = round(SOR,2)
+  SOR = str(SOR)
+
+
+  SK = qs.stats.skew(returns)
+  SK = round(SK,2)
+  SK = SK.tolist()
+  SK = str(SK)
+
+
+  KU = qs.stats.kurtosis(returns)
+  KU = round(KU,2)
+  KU = KU.tolist()
+  KU = str(KU)
+
+  TA = tail_ratio(returns)
+  TA = round(TA,2)
+  TA = str(TA)
+
+
+  CSR = qs.stats.common_sense_ratio(returns)
+  CSR = round(CSR,2)
+  CSR = CSR.tolist()
+  CSR = str(CSR)
+
+
+  VAR = qs.stats.value_at_risk(returns, sigma=sigma_value, confidence=confidence_value)
+  VAR = np.round(VAR, decimals=2)
+  VAR = str(VAR*100)+' %'
+
+  AL = alpha_beta(returns, benchmark, risk_free=rf)
+  AL = AL[0]
+  AL = round(AL,2)
+
+  BTA = alpha_beta(returns, benchmark, risk_free=rf)
+  BTA = BTA[1]
+  BTA = round(BTA,2)
+
+  def condition(x):
+    return x > 0
+
+  win = sum(condition(x) for x in returns)
+  total = len(returns)
+  win_ratio = win/total
+  win_ratio = win_ratio*100
+  win_ratio = round(win_ratio,2)
+
+  IR = information_ratio(returns, benchmark.iloc[:,0])
+  IR = round(IR,2)
+
+
+
+  data = {'':['Annual return', 'Cumulative return', 'Annual volatility','Winning day ratio', 'Sharpe ratio','Calmar ratio', 'Information ratio', 'Stability', 'Max Drawdown','Sortino ratio','Skew', 'Kurtosis', 'Tail Ratio', 'Common sense ratio', 'Daily value at risk',
+              'Alpha', 'Beta'
+
+  ],
+        'Backtest':[CAGR, CUM, VOL,f'{win_ratio}%', SR, CR, IR, STABILITY, MD, SOR, SK, KU, TA, CSR, VAR, AL, BTA]}
+
+  # Create DataFrame
+  df = pd.DataFrame(data)
+  df.set_index('', inplace=True)
+  df.style.set_properties(**{'background-color': 'white',
+                           'color': 'black',
+                          'border-color':'black'})
+
+  empyrial.df = data
+
+  if rebalance == True:
+      df
+
+  y = []
+  for x in returns:
+    y.append(x)
+
+  arr = np.array(y)
+  arr
+  returns.index
+  my_color = np.where(arr>=0, 'blue', 'grey')
+  ret = plt.figure(figsize=(30,8))
+  plt.vlines(x=returns.index, ymin=0, ymax=arr, color=my_color, alpha=0.4)
+  plt.title('Returns')
+  plt.close(ret)
+  ret.savefig('ret.png')
+  
+
+  pdf.cell(20, 7, f"", ln=1)
+  pdf.cell(20, 7, f"Annual return: " + str(CAGR), ln=1)
+  pdf.cell(20, 7, f"Cumulative return: " + str(CUM), ln=1)
+  pdf.cell(20, 7, f"Annual volatility: " + str(VOL), ln=1)
+  pdf.cell(20, 7, f"Winning day ratio: " + str(win_ratio), ln=1)
+  pdf.cell(20, 7, f"Sharpe ratio: " + str(SR), ln=1)
+  pdf.cell(20, 7, f"Calmar ratio: " + str(CR), ln=1)
+  pdf.cell(20, 7, f"Information ratio: " + str(IR), ln=1)
+  pdf.cell(20, 7, f"Stability: " + str(STABILITY), ln=1)
+  pdf.cell(20, 7, f"Max drawdown: " + str(MD), ln=1)
+  pdf.cell(20, 7, f"Sortino ratio: " + str(SOR), ln=1)
+  pdf.cell(20, 7, f"Skew: " + str(SK), ln=1)
+  pdf.cell(20, 7, f"Kurtosis: " + str(KU), ln=1)
+  pdf.cell(20, 7, f"Tail ratio: " + str(TA), ln=1)
+  pdf.cell(20, 7, f"Common sense ratio: " + str(CSR), ln=1)
+  pdf.cell(20, 7, f"Daily value at risk: " + str(VAR), ln=1)
+  pdf.cell(20, 7, f"Alpha: " + str(AL), ln=1)
+  pdf.cell(20, 7, f"Beta: " + str(BTA), ln=1)
+
+  qs.plots.returns(returns,benchmark, cumulative=True, savefig= "retbench.png", show=False)
+  qs.plots.monthly_heatmap(returns,savefig= "heatmap.png", show=False)
+  qs.plots.drawdown(returns, savefig= "drawdown.png", show=False)
+  qs.plots.drawdowns_periods(returns, savefig= "d_periods.png", show=False)
+  qs.plots.rolling_volatility(returns,savefig= "rvol.png", show=False)
+  qs.plots.rolling_sharpe(returns,savefig= "rsharpe.png", show=False)
+  qs.plots.rolling_beta(returns, benchmark,savefig= "rbeta.png", show=False)
+
+  pdf.image('ret.png', x = -20, y = None, w = 250, h = 80, type = '', link = '')
+  pdf.cell(20, 7, f"", ln=1)
+  pdf.image('retbench.png', x = -20, y = None, w = 200, h = 100, type = '', link = '')
+  pdf.cell(20, 7, f"", ln=1)
+  pdf.image('heatmap.png', x = None, y = None, w = 200, h = 80, type = '', link = '')
+  pdf.cell(20, 7, f"", ln=1)
+  pdf.image('drawdown.png', x = None, y = None, w = 200, h = 80, type = '', link = '')
+  pdf.cell(20, 7, f"", ln=1)
+  pdf.image('d_periods.png', x = None, y = None, w = 200, h = 80, type = '', link = '')
+  pdf.cell(20, 7, f"", ln=1)
+  pdf.image('rvol.png', x = None, y = None, w = 190, h = 80, type = '', link = '')
+  pdf.cell(20, 7, f"", ln=1)
+  pdf.image('rsharpe.png', x = None, y = None, w = 190, h = 80, type = '', link = '')
+  pdf.cell(20, 7, f"", ln=1)
+  pdf.image('rbeta.png', x = None, y = None, w = 190, h = 80, type = '', link = '')
+
+  pdf.output('report.pdf', 'F')
