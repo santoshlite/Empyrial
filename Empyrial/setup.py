cff69848b469d7469201cdfb3965eb495153ecea
Santosh <61618641+ssantoshp@users.noreply.github.com>
2021-07-26 20:34:26 +0200

    🌌 Stable version

--- Empyrial/setup.py
+++ Empyrial/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf8") as fh:
 
 setup(
     name='empyrial',
-    version='1.8.9',
+    version='1.9.1',
     description='An Open Source Portfolio Management Framework for Everyone 投资组合管理',
     py_modules=['empyrial'],
     package_dir={'': 'src'},
@@ -28,4 +28,3 @@ setup(
 
     ],
 )
-
