7e5d66db3aa721c1e0d4149d3f0e793efa5492d1
Santosh Passoubady <61618641+ssantoshp@users.noreply.github.com>
2023-07-04 21:57:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='empyrial',
     version='2.0.4',
     description='An Open Source Portfolio Management Framework for Everyone 投资组合管理',
-    py_modules=['src/empyrial'],
+    py_modules=['src.empyrial'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/ssantoshp/Empyrial',
