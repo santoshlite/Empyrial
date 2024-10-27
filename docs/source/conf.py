eefd3d904bcb84dfc34a96111e9dddc73f9f21f0
Santosh Passoubady <santoshpassoubady@gmail.com>
2024-10-27 11:20:26 -0400

    modules + setting up docs

---
+++ docs/source/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../EigenLedger/main.py'))
+
+project = 'EigenLedger'
+copyright = '2024, Santosh Passoubady'
+author = 'Santosh Passoubady'
+release = '2.1.5'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # For Google-style docstrings
+    'sphinx_autodoc_typehints',  # For type hinting
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+html_static_path = ['_static']
